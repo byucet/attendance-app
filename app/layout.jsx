dbff879c974f4d111e5151612c54ae9213a3093b
jakeevans <ejake370@gmail.com>
2024-04-09 21:29:51 -0600

    initialize prisma

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,7 +7,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex flex-col justify-center items-center p-10">
+        <div className="flex flex-col h-screen justify-center items-center p-10">
           <h1 className="text-3xl font-bold">Welcome to our Event!</h1>
           {children}
         </div>
